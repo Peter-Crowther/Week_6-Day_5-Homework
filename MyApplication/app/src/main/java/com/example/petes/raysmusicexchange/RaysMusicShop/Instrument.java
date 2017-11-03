4bac0a2b93a908c112600221df6102a0374ee7be
Peter Crowther <petercrowther90@hotmail.com>
2017-11-03 13:06:22 +0000

    contructed instrument abstract class

--- MyApplication/app/src/main/java/com/example/petes/raysmusicexchange/RaysMusicShop/Instrument.java
+++ MyApplication/app/src/main/java/com/example/petes/raysmusicexchange/RaysMusicShop/Instrument.java
@@ -1,10 +1,29 @@
 package com.example.petes.raysmusicexchange.RaysMusicShop;
 
+import com.example.petes.raysmusicexchange.Behaviours.Playable;
+
 /**
  * Created by petes on 03/11/2017.
  */
 
-abstract public class Instrument {
+abstract public class Instrument implements Playable {
+
+    protected String type;
+    protected String colour;
+
+    public Instrument(String type, String colour) {
+        this.type = type;
+        this.colour = colour;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public String getColour() {
+        return this.colour;
+    }
+
 
 
 }

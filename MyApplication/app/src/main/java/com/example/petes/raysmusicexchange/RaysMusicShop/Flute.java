b6daac2d49df8cce69280ecc29ee1fc527177497
Peter Crowther <petercrowther90@hotmail.com>
2017-11-03 13:36:16 +0000

    contstructed flute class

--- MyApplication/app/src/main/java/com/example/petes/raysmusicexchange/RaysMusicShop/Flute.java
+++ MyApplication/app/src/main/java/com/example/petes/raysmusicexchange/RaysMusicShop/Flute.java
@@ -1,8 +1,21 @@
 package com.example.petes.raysmusicexchange.RaysMusicShop;
 
+import com.example.petes.raysmusicexchange.Behaviours.Playable;
+
 /**
  * Created by petes on 03/11/2017.
  */
 
-public class Flute {
+public class Flute extends Instrument implements Playable {
+    int numberOfValves;
+
+    public Flute(String type, String colour, int numberOfValves) {
+        super(type, colour);
+        this.numberOfValves = numberOfValves;
+    }
+
+    @Override
+    public String play() {
+        return "Flootytoot!";
+    }
 }

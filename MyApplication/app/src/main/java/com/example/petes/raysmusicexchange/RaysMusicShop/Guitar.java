8a30aff9f665fca573a517264fcdc5833fffce8d
Peter Crowther <petercrowther90@hotmail.com>
2017-11-03 13:17:13 +0000

    contstructed guitar class

--- MyApplication/app/src/main/java/com/example/petes/raysmusicexchange/RaysMusicShop/Guitar.java
+++ MyApplication/app/src/main/java/com/example/petes/raysmusicexchange/RaysMusicShop/Guitar.java
@@ -4,8 +4,21 @@ package com.example.petes.raysmusicexchange.RaysMusicShop;
  * Created by petes on 03/11/2017.
  */
 
-public class Guitar {
+public class Guitar extends Instrument  {
 
+    int numberOfStrings;
 
+    public Guitar(String type, String colour, int numberOfStrings) {
+        super(type, colour);
+        this.numberOfStrings = numberOfStrings;
+    }
 
+    public int getNumberOfStrings(){
+        return this.numberOfStrings;
+    }
+
+    @Override
+    public String play() {
+        return "Twang";
+    }
 }
